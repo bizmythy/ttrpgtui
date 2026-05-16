43bd447dc1b9d5ee2432b1a16d34d3e7733745fe
bizmythy <andrew.p.council@gmail.com>
2026-05-16 17:14:50 -0400

    get something very very basic rendering

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use ratatui::widgets::TableState;
+
 use crate::creature::Creatures;
 
 /// Application.
@@ -7,12 +9,17 @@ pub struct App {
     pub should_quit: bool,
     /// Creature states
     pub creatures: Creatures,
+    /// Creature table state
+    pub creature_table_state: TableState
 }
 
 impl App {
     /// Constructs a new instance of [`App`].
     pub fn new() -> Self {
-        Self::default()
+        let mut app = Self::default();
+        app.creature_table_state.select_first();
+        app.creature_table_state.select_first_column();
+        app
     }
 
     /// Handles the tick event of the terminal.
--- src/ui.rs
+++ src/ui.rs
@@ -1,30 +1,40 @@
-use ratatui::{
-    Frame,
-    layout::Alignment,
-    style::{Color, Style},
-    widgets::{Block, BorderType, Borders, Paragraph},
-};
+use color_eyre::Result;
+use crossterm::event::{self, KeyCode};
+use ratatui::Frame;
+use ratatui::layout::{Alignment, Constraint, Layout, Rect};
+use ratatui::style::{Color, Style, Stylize};
+use ratatui::text::{Line, Span};
+use ratatui::widgets::{Block, BorderType, Borders, Paragraph, Row, Table, TableState};
 
 use crate::app::App;
 
 pub fn render(app: &mut App, frame: &mut Frame) {
-    let text = format!(
-        "
-      Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-      Press `j` and `k` to increment and decrement the counter respectively.\n\
-      Counter: N/A
-    ");
+    let header = Row::new(["Name", "Initiative", "Health"])
+        .style(Style::new().bold())
+        .bottom_margin(1);
 
-    let block = Block::default()
-        .title("Counter App")
-        .title_alignment(Alignment::Center)
-        .borders(Borders::ALL)
-        .border_type(BorderType::Rounded);
+    let rows = [
+        Row::new(["Eggplant", "1 medium", "25 kcal, 6g carbs, 1g protein"]),
+        Row::new(["Tomato", "2 large", "44 kcal, 10g carbs, 2g protein"]),
+        Row::new(["Zucchini", "1 medium", "33 kcal, 7g carbs, 2g protein"]),
+        Row::new(["Bell Pepper", "1 medium", "24 kcal, 6g carbs, 1g protein"]),
+        Row::new(["Garlic", "2 cloves", "9 kcal, 2g carbs, 0.4g protein"]),
+    ];
 
-    let widget = Paragraph::new(text)
-        .block(block)
-        .style(Style::default().fg(Color::Yellow))
-        .alignment(Alignment::Center);
+    let widths = [
+        Constraint::Percentage(30),
+        Constraint::Percentage(20),
+        Constraint::Percentage(50),
+    ];
 
-    frame.render_widget(widget, frame.area())
+    let table = Table::new(rows, widths)
+        .header(header)
+        .column_spacing(1)
+        .style(Color::White)
+        .row_highlight_style(Style::new().on_black().bold())
+        .column_highlight_style(Color::Gray)
+        .cell_highlight_style(Style::new().reversed().cyan())
+        .highlight_symbol("▶ ");
+    
+    frame.render_stateful_widget(table, frame.area(), &mut app.creature_table_state)
 }
